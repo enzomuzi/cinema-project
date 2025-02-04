89210c87f7c0f5724cf7d750015ac9131fba4edf
Enzo Muzi <enzshadows@gmail.com>
2025-02-04 01:03:59 -0300

    feat-01: Criação página home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BotaoHomeComponent } from './features/componentes-gerais/botao-home/botao-home/botao-home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BotaoHomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'cinema-project';
